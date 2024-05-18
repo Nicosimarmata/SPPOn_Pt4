825debe8725d1a011b2905e3ca6b57c7af85b692
Alfian57 <alfiangadingsaputra@gmail.com>
2024-05-18 17:50:04 +0700

    fix: fix authorization bug

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -3,27 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-
-class Pengguna extends Model
+class Pengguna extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
     protected $table = 'pengguna';
     protected $primaryKey = 'id_pengguna';
-    protected $guarded= ['id_pengguna'];
+    protected $guarded = ['id_pengguna'];
 
     protected $hidden = 'password';
 
-    public static function attemptLogin($username, $password){
+    public static function attemptLogin($username, $password)
+    {
         $pengguna = self::where('username', $username)->first();
 
-        if($pengguna && Hash::check($password, $pengguna->password)){
+        if ($pengguna && Hash::check($password, $pengguna->password)) {
             return $pengguna;
         }
 
         return null;
     }
 }
-
--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -3,14 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Siswa extends Model
+class Siswa extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'siswa';
     protected $primaryKey = 'id_siswa';
     protected $guard = 'id_siswa';
-
-    
 }
